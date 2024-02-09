d62e50494cfc4a0b729d315e537e27b4d1d3f610
elisabichiappini <elisabichiappini@gmail.com>
2024-02-09 01:23:52 +0100

    feat: add button

--- resources/views/admin/cocktails/index.blade.php
+++ resources/views/admin/cocktails/index.blade.php
@@ -8,6 +8,7 @@
                 <th scope="col" class="text-uppercase">Id</th>
                 <th scope="col" class="text-uppercase">Cocktail</th>
                 <th scope="col" class="text-uppercase">Brand</th>
+                <th scope="col" class="text-end"></th>
             </tr>
         </thead>
         <tbody>
@@ -16,7 +17,11 @@
                 <th>{{ $cocktail['id'] }}</th>
                 <td>{{ $cocktail['name'] }}</td>
                 <td>{{ $cocktail['brand'] }}</td>
-                <td>//bottoni</td>
+                <td class="text-end">
+                    <a href="" class="btn btn-info">Show</a>
+                    <a href="" class="btn btn-primary">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
             </tr>
             @endforeach
         </tbody>
