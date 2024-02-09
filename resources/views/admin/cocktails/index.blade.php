96029e05b1207d2c553a6e0776e62d42bfa0291b
elisabichiappini <elisabichiappini@gmail.com>
2024-02-09 01:20:08 +0100

    feat: index view dashboard

--- resources/views/admin/cocktails/index.blade.php
+++ resources/views/admin/cocktails/index.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.admin')
 
 @section('content')
-    elenco post
+    <h1>Lista Cocktails</h1>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col" class="text-uppercase">Id</th>
+                <th scope="col" class="text-uppercase">Cocktail</th>
+                <th scope="col" class="text-uppercase">Brand</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($cocktails as  $cocktail)
+            <tr>
+                <th>{{ $cocktail['id'] }}</th>
+                <td>{{ $cocktail['name'] }}</td>
+                <td>{{ $cocktail['brand'] }}</td>
+                <td>//bottoni</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
